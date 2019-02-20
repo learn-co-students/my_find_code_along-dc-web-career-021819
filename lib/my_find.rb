725be4b3619e5a8db3f2cfef0367ecb8c6f6c6df
Mickey <micksheridan.24@gmail.com>
2019-02-20 11:19:40 -0500

    submit

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,16 @@
 require 'pry'
 
 def my_find(collection)
-
+    i = 0
+    found = nil
+    while i < collection.length
+        con = yield (collection[i])
+        if (con)
+    
+            found = collection[i]
+            break
+        end
+        i+= 1
+    end
+    return found
 end
